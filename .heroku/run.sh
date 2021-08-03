4a724c7ecaed174b884f856820c45b9be2c0c95e
Ganesh-manoharan <78746251+Ganesh-manoharan@users.noreply.github.com>
2021-08-03 16:26:23 +0530

    Update run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,3 +1,9 @@
+
+mkdir sonar
+cd sonar
+wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip
+unzip sonar-scanner-cli-4.4.0.2170-linux.zip
+export PATH="/sonar/sonar-scanner-4.4.0.2170-linux/bin:$PATH"
 sonar-scanner \
   -Dsonar.organization=ganesh-manoharan \
   -Dsonar.projectKey=Ganesh-manoharan_reactapp-heroku \
