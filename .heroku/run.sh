cb3a7d3c8f073d9a4e84ea9219405100d5b42b19
Ganesh-manoharan <78746251+Ganesh-manoharan@users.noreply.github.com>
2021-09-02 20:22:39 +0530

    Update run.sh

--- .heroku/run.sh
+++ .heroku/run.sh
@@ -1,6 +1,5 @@
-wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.4.0.2170-linux.zip
-unzip sonar-scanner-cli-4.4.0.2170-linux.zip
-echo "#/bin/bash" >> sonar-scanner.sh
-echo "export PATH="$PATH:sonar-scanner-4.4.0.2170-linux/bin"" >> sonar-scanner.sh
-source sonar-scanner.sh
-sonar-scanner -Dsonar.organization=ganesh-manoharan -Dsonar.projectKey=Ganesh-manoharan_reactapp-heroku -Dsonar.sources=. -Dsonar.host.url=https://sonarcloud.io
+add-apt-repository ppa:certbot/certbot
+apt-get update
+apt-get install python-certbot-nginx
+apt-get install nginx
+certbot --nginx -d ecwid.cn.com
